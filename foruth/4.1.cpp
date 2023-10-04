14f0d886851564c56bad57a8b26b2638eb85dab1
huise0ng <shinhuiseong07@gmail.com>
2023-10-04 19:32:55 +0900

    ADD :: 4.1

---
+++ foruth/4.1.cpp
@@ -0,0 +1,23 @@
+#include <avr/wdt.h>
+
+uint32_t count_push = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(2, INPUT);
+  wdt_enable(WDTO_15MS);
+}
+
+void loop() {
+  wdt_reset();
+
+  if (digitalRead(2) == HIGH) {
+    count_push++;
+  } else {
+    if (count_push != 0) {
+      Serial.println("count_push: " + String(count_push));
+    }
+    count_push = 0;
+  }
+}
